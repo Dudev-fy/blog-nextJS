72874063cbdd3d18dbb5a82dfbcdd5ff1322a972
Dudev-fy <santos.eduardom06.03@gmail.com>
2024-07-27 01:44:32 -0300

    add untracked files

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,7 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalSyles = createGlobalStyle`
-    body {
-        background-color: red;
+    * {
+        margin: 0;
+        padding: 0;
+        outline: none;
+        box-sizing: border-box;
     }
 `;
